57d5f73e4bf4d8ec6b22422bde40da202535ae36
Howard.Zuo <leftstick@qq.com>
2016-02-14 17:36:20 +0800

    code re-formatter

--- index.js
+++ index.js
@@ -8,40 +8,41 @@ var log = require('pretty-log');
 var _ = require('lodash');
 var fs = require('fs');
 
-var fileExists = function(file) {
+var fileExists = function(file){
     try {
         return fs.statSync(file).isFile();
-    } catch (e) {
+    }catch (e){
         return false;
-    }
-}
-
-var copy = function(source, dest, data, settings) {
-    var localSettings = _.merge({clobber: true}, settings);
-
-    if (!localSettings.clobber && fileExists(dest)) {
-        return;
-    }
-
-    if (isBinaryFile.sync(source)) {
-        fse.copySync(source, dest);
-    } else {
-        fse.ensureDirSync(path.dirname(dest));
-
-        var txt = fs.readFileSync(source, {encoding: 'utf8'});
-        var out = _.template(txt, localSettings.templateOptions)(data);
-        fs.writeFileSync(dest, out, {encoding: 'utf8'});
     }
 };
 
-module.exports = function(source, destination, data, settings) {
+var copy = function(source, dest, data, settings){
+    var localSettings = _.merge({ clobber: true }, settings);
+    var txt, out;
+
+    if (!localSettings.clobber && fileExists(dest)){
+        return;
+    }
+
+    if (isBinaryFile.sync(source)){
+        fse.copySync(source, dest);
+    }else {
+        fse.ensureDirSync(path.dirname(dest));
+
+        txt = fs.readFileSync(source, { encoding: 'utf8' });
+        out = _.template(txt, localSettings.templateOptions)(data);
+        fs.writeFileSync(dest, out, { encoding: 'utf8' });
+    }
+};
+
+module.exports = function(source, destination, data, settings){
     var src = source || './';
 
-    if (!source) {
+    if (!source){
         log.warn('source isn\'t set, use "./" by default');
     }
 
-    if (!destination || destination === src) {
+    if (!destination || destination === src){
         log.error('destination cannot be empty, or the same as source');
         return;
     }
@@ -51,7 +52,7 @@ module.exports = function(source, destination, data, settings) {
         cwd: src
     });
 
-    files.forEach(function(file) {
+    files.forEach(function(file){
         var dest = path.join(destination, file);
         copy(path.join(src, file), dest, data, settings);
     });
